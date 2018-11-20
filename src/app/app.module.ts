924270d2e26c72f560ad4bf36f6b4bbc294e4316
Siddhu Nallasivam <kousiksiddhu@gmail.com>
2018-11-20 14:33:21 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
+import { LoginPage } from './../pages/login/login';
+import { ProfilePage } from './../pages/profile/profile';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TransactionPage } from '../pages/transaction/transaction';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    ProfilePage,
+    TransactionPage,
+    LoginPage,
     TabsPage
   ],
   imports: [
@@ -26,9 +28,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    ProfilePage,
+    TransactionPage,
     TabsPage
   ],
   providers: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController, LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,80 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  amountToBeSent:number=0;
+  amountRemaining:number=413;
+  subTotal=[];
+  loader;
+  vendor = "vendor";
+  
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public loadingCtrl: LoadingController) {
   }
 
+  addAmount(amt:number, img:string){
+    if(amt && (this.amountRemaining - amt) >= 0){
+      this.amountToBeSent = Number(this.amountToBeSent) + Number(amt);
+      this.amountRemaining -= amt;
+      this.subTotal.push({amount: amt, imgSrc: img});
+    }
+    else if(this.amountToBeSent && (this.amountRemaining - this.amountToBeSent) >= 0){
+      this.amountRemaining -= this.amountToBeSent;
+    }
+    else
+    {
+      (this.amountToBeSent == 0) ? this.sendToast('Add Amount!!') : this.sendToast('Insufficient funds!!');
+    }
+  }
+
+  removeAmount(index:number, amt:number){
+    this.subTotal.splice(index, 1);
+    if((this.amountToBeSent - amt) >= 0){
+      this.amountToBeSent -= amt;
+      this.amountRemaining = Number(this.amountRemaining) + Number(amt);
+    }else{
+      this.amountToBeSent = 0;
+    }
+  }
+
+  closeOtherFabs(){
+    for(var i in arguments){
+      console.log(arguments[i]);
+      arguments[i].close();
+    }
+  }
+
+  sendAmount(){
+    if(this.amountToBeSent && (this.amountRemaining - this.amountToBeSent) >= 0){
+      this.clearFields();
+      this.showLoader();
+      setTimeout(()=> {
+        this.hideLoader();
+        this.sendToast('Amount Sent!!');
+      },1000);
+    }
+    return false;
+  }
+
+  sendToast(msg){
+    const toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  clearFields(){
+    this.amountToBeSent = 0;
+    this.subTotal = [];
+  }
+
+  showLoader(){
+    this.loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    this.loader.present();
+  }
+
+  hideLoader(){
+    this.loader.dismiss();
+  }
 }
